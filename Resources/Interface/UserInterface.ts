f45cf06f6303641fd50f8fe35a90e7b587519b7e
NicEspinozaT <nic.espinozat@duocuc.cl>
2024-05-16 16:21:13 -0400

    Agregando Resources

---
+++ Resources/Interface/UserInterface.ts
@@ -0,0 +1,11 @@
+interface RegistroPersona {
+    user: {
+        username: string;
+        email: string;
+        password: string;
+    },
+    telefono: number;
+    direccion: string;
+    nombre: string;
+    apellido: string;
+}
