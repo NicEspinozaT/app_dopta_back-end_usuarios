4c9d01d47dd7961b6dc1edf2223bf008c45dc1b6
NicEspinozaT <nic.espinozat@duocuc.cl>
2024-05-27 22:58:30 -0400

    fix credenciales firebase

--- firebasestorage/firebase.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, storage
-
-cred = credentials.Certificate("./firebasestorage/firebase_key.json")
-firebase_admin.initialize_app(cred, {"storageBucket": "static-5d4d6.appspot.com"})
-
-bucket = storage.bucket()
